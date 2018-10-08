11fd4eccdc18905d017ea6a3e37aac4cf031ef3c
Joan Edimburgo <s1886255@bardal.inf.ed.ac.uk>
2018-10-08 19:16:42 +0100

    working version streaming combiner

--- task2/combiner.py
+++ task2/combiner.py
@@ -1,16 +1,27 @@
 #!/usr/bin/env python2
 import sys
 
-new_dict = {}
+def reduce_function(word, values):
+    # Calculate how many times each word was encountered
+    return word, sum(values)
+
+prev_key = None
+values = []
 
 for line in sys.stdin:
     # Parse key and value
     key, value = line.strip().split('\t')
 
-    if key in new_dict:
-        new_dict[key] = new_dict[key] + int(value)
-    else:
-        new_dict[key] = int(value)
+     # If key has changed then one can finish processing the previoos key
+    if key != prev_key and prev_key is not None:
+        result_key, result_value = reduce_function(prev_key, values)
+        print(result_key + "\t" + str(result_value))
+        values = []
 
-for new_key, new_value in new_dict.items():
-    print(new_key + "\t" + str(new_value))
+    prev_key = key
+    values.append(int(value))
+
+# Don't forget about the last value!
+if prev_key is not None:
+    result_key, result_value = reduce_function(prev_key, values)
+    print(result_key + "\t" + str(result_value))
