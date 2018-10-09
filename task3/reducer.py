382d2849d2e8c812388cb33e4776fe275e0a9e5c
Joan Pad <thevader33@gmail.com>
2018-10-09 21:46:42 +0100

    task 3 working

--- task3/reducer.py
+++ task3/reducer.py
@@ -3,16 +3,14 @@ import sys
 
 prev_key = None
 
-for line in sys.stdin:
-    # Parse key and value
-    key = line.strip().split('\t')
+for key in sys.stdin:
 
      # If key has changed then one can finish processing the previoos key
     if key != prev_key and prev_key is not None:
-        print(key +  "\t")
+        print(prev_key.strip())
 
     prev_key = key
 
 # Don't forget about the last value!
 if prev_key is not None:
-    print(prev_key +  "\t")
+    print(prev_key.strip())
